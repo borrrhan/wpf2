fe0b5e5fd4123ef9f2bf43692742d61cced93f8e
m305-50\w401 <a106082027@mail.shu.edu.tw>
2018-05-10 18:01:47 +0800

    no message

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if(result == true)
+            {
+                // 存檔 (路徑、文字內容);
+                System.IO.File.WriteAllText(dlg.FileName, TextArea.Text);
+            }
+
+            
+        }
+
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            //開檔
+            TextArea.Text = System.IO.File.ReadAllText(@"C:\git\aaa.text");
+        }
+    }
+}
